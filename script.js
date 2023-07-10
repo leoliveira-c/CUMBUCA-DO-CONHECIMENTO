6e6d21e5ba3387caf8030217800c95ee5fe62758
Léo Oliveira <leo.oliveira2015@outlook.com>
2023-07-10 18:03:02 -0300

    variables-change

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var sortearGruposButton = document.getElementById('sortear-grupos');
-    var gruposSorteados = document.getElementById('gruposSorteados');
+    let sortearGruposButton = document.getElementById('sortear-grupos');
+    let gruposSorteados = document.getElementById('gruposSorteados');
 
     sortearGruposButton.addEventListener('click', function() {
-        var grupos = document.getElementsByClassName('sorteados-group');
-        var participantesPorGrupo = [];
+        let grupos = document.getElementsByClassName('sorteados-group');
+        let participantesPorGrupo = [];
 
         // Obter todos os nomes dos participantes, separados por grupo
-        for (var i = 0; i < grupos.length; i++) {
-            var olElements = grupos[i].getElementsByTagName('ol');
-            var participantes = [];
-            for (var j = 0; j < olElements.length; j++) {
-                var nome = olElements[j].innerHTML;
+        for (let i = 0; i < grupos.length; i++) {
+            let olElements = grupos[i].getElementsByTagName('ol');
+            let participantes = [];
+            for (let j = 0; j < olElements.length; j++) {
+                let nome = olElements[j].innerHTML;
                 if (nome !== '') {
                     participantes.push(nome);
                 }
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         // Verificar se há pelo menos 2 participantes em cada grupo
-        for (var k = 0; k < grupos.length; k++) {
+        for (let k = 0; k < grupos.length; k++) {
             if (participantesPorGrupo[k].length < 2) {
                 alert('É necessário pelo menos 2 participantes no grupo ' + (k + 1) + ' para formar as duplas.');
                 return;
@@ -28,20 +28,20 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         // Embaralhar a ordem dos participantes em cada grupo
-        for (var m = 0; m < participantesPorGrupo.length; m++) {
+        for (let m = 0; m < participantesPorGrupo.length; m++) {
             participantesPorGrupo[m].sort(function() { return 0.5 - Math.random() });
         }
 
         // Criar as duplas, garantindo que não sejam do mesmo grupo
-        var duplas = [];
-        for (var n = 0; n < participantesPorGrupo.length; n++) {
-            var grupoParticipantes = participantesPorGrupo[n];
+        let duplas = [];
+        for (let n = 0; n < participantesPorGrupo.length; n++) {
+            let grupoParticipantes = participantesPorGrupo[n];
             while (grupoParticipantes.length >= 2) {
-                var participante1 = grupoParticipantes.shift();
-                var participante2 = null;
-                var participanteIndex = null;
+                let participante1 = grupoParticipantes.shift();
+                let participante2 = null;
+                let participanteIndex = null;
 
-                for (var p = 0; p < grupoParticipantes.length; p++) {
+                for (let p = 0; p < grupoParticipantes.length; p++) {
                     if (grupoParticipantes[p] !== participante1) {
                         participante2 = grupoParticipantes[p];
                         participanteIndex = p;
@@ -60,8 +60,8 @@ document.addEventListener('DOMContentLoaded', function() {
         gruposSorteados.innerHTML = '';
 
         // Adicionar as duplas à lista de grupos sorteados
-        for (var q = 0; q < duplas.length; q++) {
-            var duplaLi = document.createElement('li');
+        for (let q = 0; q < duplas.length; q++) {
+            let duplaLi = document.createElement('li');
             duplaLi.innerHTML = duplas[q].join(' e ');
             gruposSorteados.appendChild(duplaLi);
         }
